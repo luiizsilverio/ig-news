245254d80015f7bdfe2cce87191fb856a14da0c3
luiizsilverio <luiiz.silverio@gmail.com>
2021-08-01 10:29:46 -0300

    .env.local

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -5,24 +5,39 @@ import { signIn, signOut, useSession } from 'next-auth/client'
 import styles from './styles.module.scss'
 
 export function SignInButton() {
-  const [session] = useSession()
+  const [session, loading] = useSession()
 
-  return session ? (
-    <button type="button"
+  if (loading) {
+    return (
+      <button type="button"
       className={styles.signInButton}
-      onClick={() => signOut()}  
-    >
-      <FaGithub color="#04d361" />
-      {session.user.name}
-      <FiX color="737380" className={styles.closeIcon} />
-    </button>
-  ) : (
-    <button type="button"
-      className={styles.signInButton}
-      onClick={() => signIn('github')}
+      disabled={true}
     >
       <FaGithub color="#eba417" />
-      Sign in with Github
+      Aguarde...      
     </button>
-  )
+    )
+  } else {    
+    return (
+      session ? (
+      <button type="button"
+        className={styles.signInButton}
+        onClick={() => signOut()}  
+        disabled={loading}
+      >
+        <FaGithub color="#04d361" />
+        {session.user.name}
+        <FiX color="737380" className={styles.closeIcon} />
+      </button>
+    ) : (
+      <button type="button"
+        className={styles.signInButton}
+        onClick={() => signIn('github')}
+        disabled={loading}
+      >
+        <FaGithub color="#eba417" />
+        Sign in with Github
+      </button>
+    )
+  )}
 }
